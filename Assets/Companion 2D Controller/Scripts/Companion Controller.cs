32938ecd29d903698f18b2d1f486d56793b7f9d8
sinbins <acostafabian6159@gmail.com>
2024-02-23 19:54:27 -0600

    Companion Setup
    Setting up the companion controller and companion input system. No code yet just prepping everything before I start actually coding the movement.

---
+++ Assets/Companion 2D Controller/Scripts/Companion Controller.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
+public class CompanionController : MonoBehaviour
+{
+    CompanionInputActions companionMove;
+
+}

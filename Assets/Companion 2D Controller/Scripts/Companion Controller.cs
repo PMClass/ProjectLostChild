4c21f5156ec5b98cfe94c062af5bacd1b3606a41
sinbins <acostafabian6159@gmail.com>
2024-02-27 11:29:00 -0600

    Input System
    Changes in input system

--- Assets/Companion 2D Controller/Scripts/Companion Controller.cs
+++ Assets/Companion 2D Controller/Scripts/Companion Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TarodevController;
 using TreeEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,16 +8,38 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
 public class CompanionController : MonoBehaviour
 {
+    private PlayerInputActions _actions;
+    private InputAction _move;
+
     public Rigidbody2D rb;
     public CircleCollider2D circleCollider;
 
-    public GameObject companion;
+    //public GameObject companion;
     public GameObject player;
     
  
     public float rotationSpeed = 5.0f;
     [SerializeField] public float speed = 1.0f;
     public Transform myTransform;
+    private void Awake()
+    {
+        _actions = new PlayerInputActions();
+        _move = _actions.Player.Move;
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable() => _actions.Enable();
+
+    private void OnDisable() => _actions.Disable();
+
+
+    
+        
+    
+
+
+
     private void Start()
     {
      
@@ -35,19 +58,29 @@ public class CompanionController : MonoBehaviour
 
         //transform.position += transform.forward * speed * Time.deltaTime;
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        this.gameObject.GetComponent<Rigidbody2D>().MovePosition(mousePos);
-       
-       
+        // Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        // this.gameObject.GetComponent<Rigidbody2D>().MovePosition(mousePos);
+
+        // FollowPlayer();
+        ControlCompanion();
+        
         
     }
 
     private void FollowPlayer()
     {
-        companion.transform.position = Vector2.MoveTowards(companion.transform.position, player.transform.position, speed);
+        Vector3 offset = player.transform.position;
+        offset += new Vector3(3, 3, 0);
+        transform.position = Vector2.MoveTowards(transform.position, offset, speed);
     }
 
-   
+   private void ControlCompanion()
+    {
+        Vector2 inputValue = _move.ReadValue<Vector2>().normalized;
+        Vector3 input3 = new Vector3(inputValue.x, inputValue.y, 0f);
+        transform.position += input3;
+        rb.AddForce(inputValue, ForceMode2D.Force);
+    }
 
 
 }

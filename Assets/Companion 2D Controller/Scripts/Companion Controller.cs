42b9b35dbdc049d73abcba0b540b397d9d6ca465
sinbins <acostafabian6159@gmail.com>
2024-03-05 10:48:11 -0600

    Companion Controller
    moving controller

--- Assets/Companion 2D Controller/Scripts/Companion Controller.cs
+++ Assets/Companion 2D Controller/Scripts/Companion Controller.cs
@@ -5,26 +5,29 @@ using TreeEditor;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 [RequireComponent(typeof(Rigidbody2D), typeof(CircleCollider2D))]
 public class CompanionController : MonoBehaviour
 {
 
-    private PlayerInputActions _actions, _switch;
+    private PlayerInputActions _actions, tia;
     private InputAction _move;
 
     public Rigidbody2D rb;
     public CircleCollider2D circleCollider;
 
-    public GameObject player;
+    
+    public Transform player;
     public Transform followPoint;
-    [SerializeField] private float speed = 1.0f;
-    public float hoverSpeed = 2.0f;
+    public  float moveSpeed = 2.0f;
     [SerializeField] private bool isControlled;
+    public Vector2 moveInput;
     private void Awake()
     {
         isControlled = false;
         _actions = new PlayerInputActions();
+        tia = new PlayerInputActions();
         _move = _actions.Player.Move;
 
         rb = GetComponent<Rigidbody2D>();
@@ -33,13 +36,13 @@ public class CompanionController : MonoBehaviour
     private void OnEnable()
     {
         _actions.Enable();
-        _switch.Enable();
+        tia.Enable();
     }  
 
     private void OnDisable()
     {
         _actions.Disable();
-        _switch.Disable();
+        tia.Disable();
     }  
 
     
@@ -77,23 +80,49 @@ public class CompanionController : MonoBehaviour
              ControlCompanion();
          } */
 
-        ControlCompanion();
+        if (tia.Player.Switch.IsPressed()) isControlled = true;
+        else isControlled = false;
+
+        // While the companion is not controlled
+        if(!isControlled && followPoint == null)
+        {
+            // Calculate the direction towards the target
+            Vector3 direction = followPoint.position - transform.position;
+
+            // Normalize the direction to get a unit vector
+            direction.Normalize();
+
+            // Move towards the target at constant speed
+            transform.Translate(direction * moveSpeed * Time.deltaTime); 
+
+        }
+        // Companion is controlled
+        else
+        {
+            moveInput = tia.Player.Move.ReadValue<Vector2>();
+            transform.Translate(new Vector2(moveInput.x, moveInput.y) * moveSpeed * Time.deltaTime);
+            transform.position = new Vector2(Mathf.Clamp(transform.position.x, player.position.x - 3, player.position.x + 3),
+            Mathf.Clamp(transform.position.y, player.position.y - 3, player.position.y + 3));
+        }
+
+
+        // ControlCompanion();
    
         
     }
 
-    private void FollowPlayer()
+   /* private void FollowPlayer()
     {
         
         transform.position = Vector2.MoveTowards(transform.position, followPoint.position, hoverSpeed);
-    }
+    }*/
 
-   private void ControlCompanion()
+   /*private void ControlCompanion()
     {
         Vector2 inputValue = _move.ReadValue<Vector2>();
         inputValue.Normalize();
         rb.velocity = new Vector2(inputValue.x * speed, inputValue.y * speed);
-    }
+    }*/
 
 
     private void OnCollisionEnter2D(Collision2D collision)

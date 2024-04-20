b500787a55a7630addd593aab3eeead791774450
Fabian <acostafabian6159@gmail.com>
2024-04-20 16:15:41 -0500

    Explosive Enemy Completed(can be improved)

--- Assets/Scripts/Gameplay/Enemies/Explosive/ExplosiveEnemyScriptCopy.cs
+++ Assets/Scripts/Gameplay/Enemies/Explosive/ExplosiveEnemyScriptCopy.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Runtime.Serialization;
+using TarodevController;
 using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
@@ -14,102 +15,200 @@ public class ExplosiveEnemyScript : MonoBehaviour
     public float speed;
     private float direction;
     float distToPlayer;
+    bool canChase;
+    public float chaseDistance = 5f;
+    public float stopChasingDistance = 10f;
 
     public float distanceToExplode;
     public float explosiveTime;
     public float explosiveTicks;
-  
+    public float explosionDuration;
+    public bool canExplode;
 
     public float maxTime;
     public float movingTime;
     Rigidbody2D rb;
 
-    SpriteRenderer sp;
+    bool facingRight = true;
+    public SpriteRenderer bombSprite;
     public Color originalColor;
     public Color turnRed;
 
-    public GameObject explosion;
-   
+
+    bool notExploding = true;
+    public float fieldOfImpact;
+    public float forcePushBack;
+    public LayerMask layerToHit;
+
+    public Animator animator;
+
+    public PlayerController playerController;
+
+    public float xForce;
+    public float yForce;
+
     void Start()
     {
+        animator = GetComponentInChildren<Animator>();
         rb = GetComponent<Rigidbody2D>();
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        sp = GetComponent<SpriteRenderer>();
-        originalColor = sp.color;
+        bombSprite = GetComponentInChildren<SpriteRenderer>();
+        originalColor = bombSprite.color;
+        canExplode = false;     
+
+        playerController = FindObjectOfType<PlayerController>();
+
     }
-    void Update()
+    void FixedUpdate()
     {
 
-        float moveHorizontal = direction;
+        if (Vector2.Distance(transform.position, target.position) < chaseDistance)
+        {
+            canChase = true;
+            animator.SetBool("chasePlayer", true);
+        }
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f);
-        rb.velocity = movement * speed;
-        distToPlayer = Vector2.Distance(transform.position,target.position);
-        Debug.Log(movement.x +  ", " + movement.y);
-       //Debug.Log(distToPlayer);
-           
+        if (Vector2.Distance(transform.position, target.position) > stopChasingDistance)
+        {
+            canChase = false;
+            animator.SetBool("chasePlayer", false);
+        }
+
+        if (canChase)
+        {
+            float moveHorizontal = direction;
+
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f);
+            rb.velocity = movement * speed;
+
+            
+        }
+
+       
+
+        distToPlayer = Vector2.Distance(transform.position, target.position);
+       
+        // Direction is Right   
         if (target.position.x > transform.position.x)
         {
-                
-                direction = 1;
-
-            while(explosion.transform.localScale.x < 2)
+            direction = 1;
+            if (!facingRight && notExploding)
             {
-                explosion.transform.localScale += new Vector3(0.1f, 0.1f) * Time.deltaTime;
-            }
-                
-            if(distToPlayer <= distanceToExplode)
-            {                           
-                StartCoroutine(Explode());
-              
-            }
-           /* else
-            {
-                sp.color = originalColor;
-            } */
-
+                Flip();
+            }       
+          
         }
 
             
-        
+        // Direction is left
         if (target.position.x < transform.position.x)
+        {
+            direction = -1;
+            if (facingRight && notExploding)
             {
-
-                direction = -1;
-                 
-            if(distToPlayer <= distanceToExplode) { sp.color = turnRed; }
+                Flip();
+            }          
                 
+        }
 
-            }
+        if (distToPlayer <= distanceToExplode)
+        {
+            animator.SetBool("chasePlayer", false);          
+            StartCoroutine(Explode());
+        }
+    }
 
-           
-        
-        
+    void Flip()
+    {
+        Vector3 currentScale = gameObject.transform.localScale;
+        currentScale.x *= -1;
+        gameObject.transform.localScale = currentScale;
+        facingRight = !facingRight;
     }
 
    
+    private void Explosion()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldOfImpact, layerToHit);
+
+        foreach(Collider2D obj in objects)
+        {
+           // Vector2 direction = obj.transform.position - transform.position;
+
+            if(obj.tag == "Player")
+            {
+                if (facingRight)
+                {
+                    playerController.AddFrameForce(new(xForce, yForce), true);
+                 
+                }
+
+                else
+                {
+                    playerController.AddFrameForce(new(-xForce, yForce), true);
+                }
+              
+            }
+        }
+    }
+
 
     IEnumerator Explode()
     {
-
+        
+        notExploding = false;
         movingTime += Time.deltaTime;
+           
+        if(transform.position.x > target.position.x)
+        {
+            facingRight = false;
+        }
+        if(transform.position.x < target.position.x)
+        {
+            facingRight = true;
+        }
+
         if (movingTime >= maxTime)
         {
+            animator.SetTrigger("explode");
             for (int i = 0; i < explosiveTicks; i++)
             {
+               
                 rb.constraints = RigidbodyConstraints2D.FreezePositionX;
                 movingTime = 0;
-                sp.color = turnRed;     
+                bombSprite.color = turnRed;     
                 yield return new WaitForSeconds(explosiveTime);
-                sp.color = originalColor;
+                bombSprite.color = originalColor;
                 yield return new WaitForSeconds(explosiveTime);
-            }
-            sp.color = turnRed;
-            yield return new WaitForSeconds(explosiveTime);
+            }           
+            
+            canExplode = true;
+            
+        }
+        if (canExplode)
+        {
+          
+            bombSprite.color = turnRed;
+            Explosion();
+            yield return new WaitForSeconds(0.75f);
+            bombSprite.color = turnRed;
             Destroy(gameObject);
         }
        
 
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Explosion Radius Drawn Out
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, fieldOfImpact);
+
+        // Distance Check to Explode
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, distanceToExplode);
 
     }
+
+   
 }

99af78de0d76a2ac83ede3c515ac31b6b339c7b1
Payton <PAMCCLUNG@my.lonestar.edu>
2024-04-30 10:33:01 -0500

    Player Audio States set up
    Fall Damage audio, Long Fall audio, Death audio.

--- Assets/Scripts/Gameplay/Player/PlayerConditions.cs
+++ Assets/Scripts/Gameplay/Player/PlayerConditions.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using TarodevController;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class PlayerConditions : MonoBehaviour
 {
     private PlayerController _playerCtrl;
@@ -30,6 +32,11 @@ public class PlayerConditions : MonoBehaviour
     [field: SerializeField] public bool UseNumericHealth { get; private set; } = true;
     [field: SerializeField] public float PlayerHealth { get; private set; } = 3f;
 
+    AudioSource damageAudioPlayer;
+    public AudioClip deathAudioClip;
+    public AudioClip fallingAudioClip;
+    public AudioClip longFallDamageAudioClip;
+
     private void Awake()
     {
         InitialPos = transform.position;
@@ -43,6 +50,7 @@ public class PlayerConditions : MonoBehaviour
     void Start()
     {
         PlayerSpawn();
+        damageAudioPlayer = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -59,6 +67,8 @@ public class PlayerConditions : MonoBehaviour
         PlayerDead = true;
         PlayerHurt = true;
         if (UseNumericHealth) CurrentHealth = 0f;
+        damageAudioPlayer.PlayOneShot(deathAudioClip);
+
     }
     [ContextMenu("Respawn Player")]
     public void PlayerSpawn()
@@ -159,11 +169,14 @@ public class PlayerConditions : MonoBehaviour
 
     public void PlayerCheckFall()
     {
+        damageAudioPlayer.PlayOneShot(fallingAudioClip);
         if (!PlayerDead)
         {
             float _height = _playerCtrl.FallHeight;
             if (_height >= HeightDeathMin) { PlayerDie(); }
-            else if (_height >= HeightHurtMin) { PlayerGiveHurt(); }
+            else if (_height >= HeightHurtMin) { PlayerGiveHurt();
+                damageAudioPlayer.PlayOneShot(longFallDamageAudioClip);
+            }
         }
     }
 

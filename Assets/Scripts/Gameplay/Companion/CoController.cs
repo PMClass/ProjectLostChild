c24a26fe9fbc2145cbcc643affb5e534cd1d2846
scriptingtest <96097017+scriptingtest@users.noreply.github.com>
2024-05-03 20:54:21 -0500

    Companion Control - Max Distance
    Script CoController
    - Add public float MaxDistanceFromPlayer
    - Start function creates "anchor" GameObject with DistanceJoint2D; limits companion's maximum distance to MaxDistanceFromPlayer

--- Assets/Scripts/Gameplay/Companion/CoController.cs
+++ Assets/Scripts/Gameplay/Companion/CoController.cs
@@ -44,14 +44,31 @@ public class CompanionController : MonoBehaviour
         }
         
     }
+
+    private GameObject _anchor;
+    private Rigidbody2D _anchorRb;
+    private DistanceJoint2D _anchorJoint;
+
+    private void Start()
+    {
+        _anchor = new("Anchor");
+        _anchorRb = _anchor.AddComponent<Rigidbody2D>();
+        _anchorRb.isKinematic = true;
+
+        _anchorJoint = _anchor.AddComponent<DistanceJoint2D>();
+        _anchorJoint.maxDistanceOnly = true;
+        _anchorJoint.autoConfigureDistance = false;
+        _anchorJoint.distance = MaxDistanceFromPlayer;
+        _anchorJoint.connectedBody = _coRigid;
+    }
     #endregion
-    
+
     public void ToggleControlInteractable()
     {
         if (!InControl)
         {
             GameObject InteractableTouched = _coSense.GetInteractable();
-            if (InteractableTouched != null)
+            if (InteractableTouched != null && !tooFar)
             {
                 var tempMonoArray = InteractableTouched.GetComponents<MonoBehaviour>();
 
@@ -90,19 +107,25 @@ public class CompanionController : MonoBehaviour
     {
         if (InControl && HasControllableObj())
         {
+            _anchorJoint.enabled = false;
             // send vel to Interact function
             InteractableControlled.Interact(vel);
             _coRigid.MovePosition(InteractableObject.transform.position);
         }
         else
         {
+            _anchorJoint.enabled = true;
             // add force using vel when there is player control
-            if (!noInput) _coRigid.AddForce(vel, ForceMode2D.Impulse);
+            if (!noInput)
+            {
+                _coRigid.AddForce(vel, ForceMode2D.Impulse);
+                
+            }
             else
             {
                 // if the CoSensor is touching an interactable, reposition to its center
                 var InteractableObj = _coSense.GetInteractable();
-                if (InteractableObj != null)
+                if (InteractableObj != null && !tooFar)
                 {
                     _coRigid.MovePosition(InteractableObj.transform.position);
                 }
@@ -113,8 +136,18 @@ public class CompanionController : MonoBehaviour
         }
     }
 
+    private void FixedUpdate()
+    {
+        DistanceCheck();
+    }
+
+    bool tooFar;
     void DistanceCheck()
     {
-
+        _anchor.transform.position = transform.position;
+        
+        float _cDistance = Vector2.Distance(gameObject.transform.position, _coObject.transform.position);
+        float excess = _cDistance - MaxDistanceFromPlayer * 0.9f;
+        tooFar = excess > 0.0f;
     }
 }

eeb7284b7d113d03c703e781d91ab567685fc7ef
scriptingtest <96097017+scriptingtest@users.noreply.github.com>
2024-05-05 20:48:58 -0500

    Camera Manager - Initialize Virtual Camera
    Script CameraManager
    - Implement LoadVirtualCamera
    
    TODO
    - Fix camera not showing GameScene
    - Properly unset references from virtual camera related components
    - Create adjustable inspector value for target group objects' weight (player/companion)
    - Allow adding additional objects to target group (list that is cleared by unload function)
    - Something else that's really important, but I forgot so this is the end ok

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -9,11 +9,23 @@ public class CameraManager : MonoBehaviour
 
     GameObject playerPrefab;
     GameObject coPrefab;
-    
+    Camera cam;
+    GameObject cameraObj;
+
     // Start is called before the first frame update
     void Start()
     {
         gameManager = GameManager.Instance;
+        cam = GetComponentInChildren<Camera>();
+        if (cam != null)
+        {
+            cameraObj = cam.gameObject;
+        }
+        else
+        {
+            Debug.LogWarning("No Camera object!");
+            enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -24,10 +36,34 @@ public class CameraManager : MonoBehaviour
 
     #region Virtual Camera Functions
     CinemachineVirtualCamera virtualCamera;
-    GameObject cameraObj;
+    CinemachineGroupComposer composer;
+    CinemachineTargetGroup group;
     public void LoadVirtualCamera()
     {
-        
+        playerPrefab = gameManager.CurrentPlayer;
+        coPrefab = gameManager.CurrentCompanion;
+
+        if (playerPrefab != null && coPrefab != null)
+        {
+            if (virtualCamera == null)
+            {
+                virtualCamera = cameraObj.AddComponent<CinemachineVirtualCamera>();
+            }
+
+            if (composer == null)
+            {
+                composer = virtualCamera.AddCinemachineComponent<CinemachineGroupComposer>();
+            }
+
+            if (group == null)
+            {
+                group = cameraObj.AddComponent<CinemachineTargetGroup>();
+            }
+
+            group.AddMember(playerPrefab.transform, 1, 10);
+            group.AddMember(coPrefab.transform, 1, 10);
+
+        }
     }
 
     public void UnloadVirtualCamera()
